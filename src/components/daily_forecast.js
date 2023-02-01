2845ee45d103a0ca859cc3187a2ee38e395de8a5
zashita <mihail.trutsko@gmail.com>
2023-02-01 11:26:00 +0300

    reworked date check in daily forecast

--- src/components/daily_forecast.js
+++ src/components/daily_forecast.js
@@ -17,8 +17,9 @@ const DailyForecast = (props) => {
 
     const currentDate = new Date();
     const forecast_array = [...props.weatherObjects.filter(item=>
-    currentDate.getDate() !== Number(item.dt_txt.split(` `).shift().split("-").pop())// Comparing Dates
-)] //TODO: if possible rewrite using UNIX format
+    // currentDate.getDate() !== Number(item.dt_txt.split(` `).shift().split("-").pop())// Comparing Dates
+        currentDate.getDate() !== new Date(item.dt *1000).getDate()
+)]
     const temps = [...forecast_array.map((item)=>{
         return item.main.temp;
     })];
